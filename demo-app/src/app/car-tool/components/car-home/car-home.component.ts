2cd2076806c9ae7cb409a6f917f266269afaf581
Eric Greene <eric@t4d.io>
2022-06-01 15:05:52 -0400

    add exercise 2 solution

--- demo-app/src/app/car-tool/components/car-home/car-home.component.ts
+++ demo-app/src/app/car-tool/components/car-home/car-home.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Car } from '../../models/cars';
+
 @Component({
   selector: 'app-car-home',
   templateUrl: './car-home.component.html',
   styleUrls: ['./car-home.component.css']
 })
 export class CarHomeComponent implements OnInit {
+
+  headerText = "Car Tool";
+
+  cars: Car[] = [
+    { id: 1, make: 'Tesla', model: 'S', year: 2020, color: 'red', price: 120000 },
+    { id: 2, make: 'Ford', model: 'T', year: 1922, color: 'black', price: 800 },
+  ]
 
   constructor() { }
 
