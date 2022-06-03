d4ae514425f8cc77196efb7cd0c96af19b539c88
Eric Greene <eric@t4d.io>
2022-06-03 12:29:02 -0400

    add rxjs error handling and better refresh

--- demo-app/src/app/color-tool/components/color-home/color-home.component.ts
+++ demo-app/src/app/color-tool/components/color-home/color-home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { switchMap } from 'rxjs';
+import { switchMap, tap, catchError, of } from 'rxjs';
 
 import { Color, NewColor } from '../../models/colors';
 import { ColorsDataService } from '../../services/colors-data.service';
@@ -13,33 +13,37 @@ import { ColorsDataService } from '../../services/colors-data.service';
 export class ColorHomeComponent implements OnInit {
 
   headerText = "Color Tool";
-
   colors: Color[] = [];
+  errorMessage = "";
 
-  constructor(private colorsData: ColorsDataService) {
-
-  }
+  constructor(private colorsData: ColorsDataService) {}
 
   ngOnInit(): void {
+    this.doRefreshColors().subscribe();
+  }
 
-    // this.colors = this.colorsData.all();
-
-    this.colorsData.all().subscribe({
-      next: colors => this.colors = colors,
-    });
+  doRefreshColors() {
+    this.errorMessage = "";
+    return this.colorsData.all().pipe(
+     catchError(err => {
+      this.errorMessage = err.message;
+      return of([]);
+     }),
+     // perform a side-effect
+     tap(colors => {
+       // the side-effect from the perspective of the observable pipeline
+       // is to change the state of the component
+      this.colors = colors;
+     }),
+    );
   }
 
   doAddColor(color: NewColor) {
-
     this.colorsData.append(color)
       .pipe(
-        switchMap(() => this.colorsData.all())
+        switchMap(() => this.doRefreshColors())
       )
-      .subscribe({
-        next: colors => this.colors = colors,
-      });
-
-
+      .subscribe();
   }
 
 }
